95b302aa9bbe9e4a54b7f93a05e809be1bcf33e5
EURICO RANDY THEOSYMEION HARSAYA (00000028869) <eurico.harsaya@student.umn.ac.id>
2021-03-10 11:10:16 +0700

    week 6

---
+++ Week06B_28869/app/src/main/java/id/ac/umn/week06b_28869/MainActivity.java
@@ -0,0 +1,26 @@
+package id.ac.umn.week06b_28869;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    AnimationDrawable animasiKuda;
+    ImageView gambarKuda;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gambarKuda = (ImageView) findViewById(R.id.gambarKuda);
+        gambarKuda.setBackgroundResource(R.drawable.kuda_lari);
+        animasiKuda = (AnimationDrawable) gambarKuda.getBackground();
+        gambarKuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animasiKuda.start();
+            }
+        });
+    }
+}
